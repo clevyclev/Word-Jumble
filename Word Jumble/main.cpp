7ae84832dfb4585a05bc504138b13e0bb8951a29
Clevis Necol Valentin Willrich <Hotness@The-Good-Mac.local>
2018-01-11 02:49:22 -1000

    First Commit

--- Word Jumble/main.cpp
+++ Word Jumble/main.cpp
@@ -1,15 +1,79 @@
-//
-//  main.cpp
-//  Word Jumble
-//
-//  Created by Clevis Necol Valentin Willrich on 2/5/17.
-//  Copyright © 2017 Clevis Necol Valentin Willrich. All rights reserved.
-//
+// Word Jumble
+// The classic word jumble game where the player can ask for a hint
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    enum fields {WORD, HINT, NUM_FIELDS};
+    const int NUM_WORDS = 5;
+    const string WORDS[NUM_WORDS][NUM_FIELDS] =
+    {
+        {"wall", "Do you feel you're banging your head against something?"},
+        {"glasses", "These might help you see the answer."},
+        {"labored", "Going slowly, is it?"},
+        {"persistent", "Keep at it."},
+        {"jumble", "It's what the game is all about."}
+    };
+    
+    srand(static_cast<unsigned int>(time(0)));
+    int choice = (rand() % NUM_WORDS);
+    string theWord = WORDS[choice][WORD];  // word to guess
+    string theHint = WORDS[choice][HINT];  // hint for word
+    
+    string jumble = theWord;  // jumbled version of word
+    int score = theWord.size(); // starting score
+    int length = jumble.size();
+    for (int i=0; i<length; ++i)
+    {
+        int index1 = (rand() % length);
+        int index2 = (rand() % length);
+        char temp = jumble[index1];
+        jumble[index1] = jumble[index2];
+        jumble[index2] = temp;
+    }
+    
+    cout << "\t\t\tWelcome to Word Jumble!\n\n";
+    cout << "Unscramble the letters to make a word.\n";
+    cout << "Enter 'hint' for a hint.\n";
+    cout << "Enter 'quit' to quit the game.\n\n";
+    cout << "The jumble is: " << jumble;
+    
+    string guess;
+    cout << "\nYour current score is " << score;
+    cout << "\n\nYour guess: ";
+    cin >> guess;
+    
+    while ((guess != theWord) && (guess != "quit"))
+    {
+        if (guess == "hint")
+        {
+            cout << theHint;
+            cout << "\nThis hint has cost you one point.";
+            score -= 1;
+            cout << "\nYour current score is " << score;
+        }
+        else
+        {
+            cout << "Sorry, that's not it.";
+        }
+        
+        cout <<"\n\nYour guess: ";
+        cin >> guess;
+    }
+    
+    if (guess == theWord)
+    {
+        cout << "\nThat's it!  You guessed it!\n";
+        cout << "Your final score is " << score;
+    }
+    
+    cout << "\nThanks for playing.\n";
+    
     return 0;
 }
